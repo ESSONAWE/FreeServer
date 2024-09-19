327633d6a2cd0e0697b0dfb362c04b8256ec1cfe
ESSONAWE <alikiessonawe@gmail.com>
2024-09-19 21:28:07 +0200

    Add files via upload

---
+++ SITE.js
@@ -0,0 +1,38 @@
+const changerAccueilh3 = document.querySelector(".AH");
+changerAccueilh3.addEventListener("mouseover", () => {
+    changerAccueilh3.style.background = "white";
+});
+changerAccueilh3.addEventListener("mouseout", () => {
+    changerAccueilh3.style.background = "";
+});
+console.log(changerAccueilh3);
+
+
+const changercouleurh3 = document.querySelector(".BH");
+changercouleurh3.addEventListener("mouseover", () => {
+    changercouleurh3.style.background = "white";
+});
+changercouleurh3.addEventListener("mouseout", () => {
+    changercouleurh3.style.background = "";
+});
+console.log(changercouleurh3);
+
+
+const changercouleurCVh3 = document.querySelector(".CV");
+changercouleurCVh3.addEventListener("mouseover", () => {
+    changercouleurCVh3.style.background = "white";
+});
+changercouleurCVh3.addEventListener("mouseout", () => {
+    changercouleurCVh3.style.background = "";
+});
+console.log(changercouleurCVh3);
+
+
+const changercouleurCHh3 = document.querySelector(".CH");
+changercouleurCHh3.addEventListener("mouseover", () => {
+    changercouleurCHh3.style.background = "white";
+});
+changercouleurCHh3.addEventListener("mouseout", () => {
+    changercouleurCHh3.style.background = "";
+});
+console.log(changercouleurCHh3);
